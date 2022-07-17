0c0d37a0be79cf690554c4902fd8b6b65c86866a
Bogonko Eddy <eddy.bogonko@strathmore.edu>
2022-07-17 14:36:50 +0300

    Deploy smart contracts

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/3XY0X7WRWEtWuA79HJtmaKbcl2ljlE8h",
+      accounts: [
+        "f12c5116ff96e95ddea051dfdf431a8e7563d1feb8271d4b1092c4c5c4f3cde4",
+      ],
+    },
+  },
 };
